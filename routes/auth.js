79d3fe6c8bee77367b9da19c2d27463f0838592f
YassineAzdoud <yassinelife2030@gmail.com>
2022-01-30 00:04:20 +0100

    add register and login

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const User = require("../model/User");
 const bcrypt = require("bcryptjs");
-const Joi = require("@hapi/joi")
+const {registerValidation,loginValidation} = require("../validation");
+const res = require("express/lib/response");
+const JWT = require("jsonwebtoken")
 
 router.get("/", (req, res) => {
     res.send("inside the home")
@@ -9,21 +11,22 @@ router.get("/", (req, res) => {
 
 
 router.post("/register", async (req, res) => {
+    
+    const {error} = registerValidation(req.data);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const checkdata = await User.findOne({email : req.body.email,})
+    if (checkdata) return res.status(400).send("Email already exists")
+
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(req.body.password, salt)
-    
-    const schema = Joi.object({
-        name: Joi.string().min(6).required(),
-        email: Joi.string().min(6).required().email,
-        password: Joi.string().min(6).required(),
-    })
-
+ 
     const user = new User({
         name : req.body.name,
         email : req.body.email,
         password : hashedPassword
     });
-// res.send('zzee')
+ 
     try {
         const savedUser = await user.save();
         res.status(200).send({user: savedUser})
@@ -35,5 +38,16 @@ router.post("/register", async (req, res) => {
     }
 });
 
+
+router.post("/login", async (req, res) =>{
+    const {error} = loginValidation(req.data);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const user1 = await User.findOne({email : req.body.email,})
+    if (!user1) return res.status(400).send("Email already exists")
+
+    const validPass = await bcrypt.compare(req.body.password, user1.password);
+    if (!validPass) return res.status(400).send("Password valid")
+})
 
 module.exports = router;
